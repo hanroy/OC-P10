53c8b5cc757c27707b521e612c693c45e08b0a0f
Han Roy <28631866+hanroy@users.noreply.github.com>
2020-07-27 22:49:56 +0200

    add some echo to entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+echo "update os"
 sudo yum update -y
+
+echo "install docker"
 sudo yum install docker -y
 sudo service docker start
-sudo service docker status
-sudo usermod -a -G docker ec2-user
 sudo systemctl enable docker
+
+echo "add ec2-user to the Docker users group"
+#Doing this allows you to run Docker commands without needing to invoke sudo every time.
+sudo usermod -a -G docker ec2-user
+
+echo "download and apply the docker-compose package"
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+echo "Set the execution permissions docker-compose"
 sudo chmod +x /usr/local/bin/docker-compose
+
+echo "install wordpress"
 mkdir wordpress && cd wordpress
 sudo wget https://raw.githubusercontent.com/hanroy/OC-P10/master/docker/Dockerfile
 sudo https://raw.githubusercontent.com/hanroy/OC-P10/master/docker/docker-compose.yml
